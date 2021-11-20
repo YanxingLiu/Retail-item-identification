ee8326977fcf8688a3438d4f664c51d9299af14d
YanxingLiu <yanxingliu@tju.edu.cn>
2021-11-20 16:43:11 +0800

    add resnet101

--- src/model_utils/device_adapter.py
+++ src/model_utils/device_adapter.py
@@ -15,8 +15,12 @@
 
 """Device adapter for ModelArts"""
 
-from .config import config
-from .local_adapter import get_device_id, get_device_num, get_rank_id, get_job_id
+from src.model_utils.config import config
+
+if config.enable_modelarts:
+    from src.model_utils.moxing_adapter import get_device_id, get_device_num, get_rank_id, get_job_id
+else:
+    from src.model_utils.local_adapter import get_device_id, get_device_num, get_rank_id, get_job_id
 
 __all__ = [
     "get_device_id", "get_device_num", "get_rank_id", "get_job_id"
